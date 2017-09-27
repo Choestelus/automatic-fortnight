0b9420d99661195765ebac36053ff8018945c908
Nattapong Amornbunchornvej <sprintf.null@gmail.com>
2017-09-27 17:42:26 +0700

    fix package ref

--- main.go
+++ main.go
@@ -2,11 +2,9 @@ package fortnight
 
 import (
 	"fmt"
-
-	"github.com/Choestelus/automatic-fortnight"
 )
 
 func main() {
 	fmt.Println("such is")
-	fmt.Println(fortnight.AddNumber(2, 3))
+	fmt.Println(AddNumber(2, 3))
 }
